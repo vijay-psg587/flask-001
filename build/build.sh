2f861bc64230529a5dd42b7197cf4cf026721eca
vijay-psg587 <vijayakumars@deloitte.com>
2022-03-15 14:07:08 +0530

    Added a build step

---
+++ build/build.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Installing npm & serverless dependencies"
+# npm init
+# Make sure serverless is installed first
+# npm i -g serverless
+npm i -g serverless
+serverless plugin install -n serverless-python-requirements
+serverless plugin install -n serverless-wsgi
+serverless plugin install -n serverless-deployment-bucket
+
+# echo "###Replacing plugin section in serverless"
+
+# sed -i  "" "s/\##plugin_name_1/\- serverless-python-requirements/g" serverless.yml
+# sed -i "" "s/\#service_name/service: iot-basic-\$\{env\:LINKER_NAME\}/g" serverless.yml
