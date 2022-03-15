3abbaaf1a8c605f298e987b01ffe0c9c034b2837
vijay-psg587 <vijayakumars@deloitte.com>
2022-03-15 14:39:15 +0530

    Added few ignore & modified serverless

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,12 @@
-#! /bin/bash
+#! /bin/zsh
 
 echo "Installing npm & serverless dependencies"
 # npm init
 # Make sure serverless is installed first
 # npm i -g serverless
-npm i -g serverless
-serverless plugin install -n serverless-python-requirements
+# npm i -g serverless
 serverless plugin install -n serverless-wsgi
-serverless plugin install -n serverless-deployment-bucket
+
 
 # echo "###Replacing plugin section in serverless"
 
